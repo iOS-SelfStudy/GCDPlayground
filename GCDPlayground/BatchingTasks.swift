e2c8759f4861bf067cd05e4424aa7e351db18e6b
mohamed korany <mkorany@victorylink.com>
2020-09-09 18:10:03 +0200

    first commit

---
+++ GCDPlayground/BatchingTasks.swift
@@ -0,0 +1,9 @@
+//
+//  BatchingTasks.swift
+//  GCDPlayground
+//
+//  Created by Mohamed Korany on 9/9/20.
+//  Copyright © 2020 Mohamed Korany. All rights reserved.
+//
+
+import Foundation
---
+++ GCDPlayground/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  GCDPlayground
+//
+//  Created by Mohamed Korany on 9/9/20.
+//  Copyright © 2020 Mohamed Korany. All rights reserved.
+//
+
+import Foundation
